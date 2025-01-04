2616afaf16043f7e9ad33ac955da48c569eeef74
me_me <ichshlanger2@gmail.com>
2025-01-04 22:44:43 +0300

    enh: data preprocessing added

--- prepare.py
+++ prepare.py
@@ -1,10 +1,34 @@
 import numpy
+from sklearn.preprocessing import OneHotEncoder
 import pandas
 import matplotlib.pyplot as plt
 
-
+TRAIN_TEST_RATIO = 0.8
 data_path = "data/raw_iris.csv"
 
 df = pandas.read_csv(data_path)
 
-print(df.head())
+###############
+# PREPROCESSING
+###############
+
+
+labels_df = pandas.DataFrame(df["variety"])
+labels = ["Setosa", "Versicolor", "Virginica"]
+
+encoder = OneHotEncoder(sparse_output=False)
+
+encoded_data = encoder.fit_transform(labels_df)
+
+encoded_df = pandas.DataFrame(encoded_data, columns=labels)
+
+df = df.drop(["variety"], axis=1)
+
+df = pandas.concat([df, encoded_df], axis=1)
+
+
+###############
+# TRAIN TEST SPLITTING
+###############
+
+df.to_csv("data/processed_iris.csv", index=False)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <torch/torch.h>
+
+
+class IrisDataset : public torch::data::Dataset<IrisDataset> {
+private:
+  std::vector<std::pair<torch::Tensor, torch::Tensor>> _data;
+
+public:
+  IrisDataset(const std::string& csv_file) {
+    load_csv(csv_file);
+  }
+
+  void load_csv(const std::string& csv_file);
+
+  torch::data::Example<> get(size_t index) override {
+    return {_data[index].first, _data[index].second};
+  }
+
+  size_t size() override {
+    return _data.size();
+  }
+};
+
 
 int main() {
   #ifdef DEBUG_MODE
   std::cout << "DEBUG_MODE ACTIVATED\n";
   #endif
+
+  std::string trainDataPath {"data/train_data.csv"};
+  std::string testDataPath {"data/test_data.csv"};
+
+  IrisDataset trainDataset(trainDataPath);
+  IrisDataset testDataset(testDataPath);
 
   return 0;
 }
